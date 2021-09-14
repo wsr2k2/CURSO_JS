a63acb812d5d826be92c4e4e6a5a15ff1bc68bbb
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-09-14 10:47:12 -0300

    Create SistemaBanco.js

---
+++ Desafios/SistemaBanco.js
@@ -0,0 +1,24 @@
+let nome = "William";
+let sobrenome = "Rodrigues";
+let idade = 39;
+const numCliente = 65466;
+let valorEmprestimo = 150000;
+let taxaDeJuros = 0.10;
+let numAnos = 6;
+let ehBomPagador = false;
+
+// montante = valorEmprestimo + juros
+// juros = valorEmprestimo * taxaDeJuros * numAnos
+
+// let juros = valorEmprestimo * taxaDeJuros * numAnos;
+// let montante = valorEmprestimo + juros;
+// console.log(montante)
+
+// ESTRUTURAS CONDIONAIS
+// bom pagador ou nAnother
+if(ehBomPagador){
+    console.log("O cliente " + nome + " é um bom pagador.");
+} else{
+    console.log("O cliente " + nome + " não é um bom pagador.")
+}
+
